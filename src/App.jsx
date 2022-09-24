4868c34f622944a753b009d8d0190e35555ec3d0
GaRev-Dev <revelo.gabriel.38@gmail.com>
2022-09-23 19:37:51 -0500

    Base proyecto React

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto min-h-screen">
+      <button className="btn">Portafolio</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#D90429",
+          secondary: "#8D99AE",
+          accent: "#EF233C",
+          neutral: "#2B2D42",
+          "base-100": "#EDF2F4",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
